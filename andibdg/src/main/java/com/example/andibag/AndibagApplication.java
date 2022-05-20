91f07b85fcedb27a711d3fd3cbf608dc93a35ad7
munjeongmin <3330kys@dsm.hs.kr>
2022-05-20 11:36:57 +0900

    firstcommit

---
+++ andibdg/src/main/java/com/example/andibag/AndibagApplication.java
@@ -0,0 +1,13 @@
+package com.example.andibag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndibagApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AndibagApplication.class, args);
+    }
+
+}
