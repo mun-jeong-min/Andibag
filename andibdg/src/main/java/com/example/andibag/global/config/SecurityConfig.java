0b65db273251552d2168e2bbc4b1ff388179abdf
munjeongmin <3330kys@dsm.hs.kr>
2022-05-23 19:03:16 +0900

    #2 WebSecurityConfiguerAdapter error

--- andibdg/src/main/java/com/example/andibag/global/config/SecurityConfig.java
+++ andibdg/src/main/java/com/example/andibag/global/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.example.andibag.global.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -9,4 +10,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
 }
