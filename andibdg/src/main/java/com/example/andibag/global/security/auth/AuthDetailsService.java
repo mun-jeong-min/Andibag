100a875e8a63cf07e867d704cde25b644951301b
munjeongmin <3330kys@dsm.hs.kr>
2022-05-23 23:02:16 +0900

    [ADD] AuthDetails ADD

---
+++ andibdg/src/main/java/com/example/andibag/global/security/auth/AuthDetailsService.java
@@ -0,0 +1,22 @@
+package com.example.andibag.global.security.auth;
+
+import com.example.andibag.domain.user.domain.repository.UserRepository;
+import com.example.andibag.domain.user.exception.UserNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class AuthDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String accountId) throws UsernameNotFoundException {
+        return userRepository.findByAccountId(accountId)
+                .map(AuthDetails::new)
+                .orElseThrow(() -> UserNotFoundException.EXCEPTION);
+    }
+}
