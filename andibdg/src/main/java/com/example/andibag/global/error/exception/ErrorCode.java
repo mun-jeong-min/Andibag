bfaf65bdaa0410955f492dbdbf4168b11d7a0446
munjeongmin <3330kys@dsm.hs.kr>
2022-05-23 18:46:59 +0900

    [ADD] errorCode

---
+++ andibdg/src/main/java/com/example/andibag/global/error/exception/ErrorCode.java
@@ -0,0 +1,14 @@
+package com.example.andibag.global.error.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+    USER_NOT_FOUND(404, "USER-404-1", "User Not Found");
+
+    private final int status;
+    private final String code;
+    private final String message;
+}
