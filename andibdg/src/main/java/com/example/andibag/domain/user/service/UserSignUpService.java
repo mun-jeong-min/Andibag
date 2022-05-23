3a73f6542a0a8b1a389ed0ea221d0f0d52dc2d44
munjeongmin <3330kys@dsm.hs.kr>
2022-05-23 23:07:59 +0900

    [EDIT] PasswordEncoder

--- andibdg/src/main/java/com/example/andibag/domain/user/service/UserSignUpService.java
+++ andibdg/src/main/java/com/example/andibag/domain/user/service/UserSignUpService.java
@@ -6,12 +6,14 @@ import com.example.andibag.domain.user.exception.UserExistException;
 import com.example.andibag.domain.user.present.dto.request.SignUpRequest;
 import com.example.andibag.global.enums.Authority;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class UserSignUpService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public void signup(SignUpRequest request) {
         if(userRepository.findByAccountId(request.getAccountId()).isPresent())
@@ -20,7 +22,7 @@ public class UserSignUpService {
         userRepository.save(
                 User.builder()
                         .accountId(request.getAccountId())
-                        .password(request.getPassword())
+                        .password(passwordEncoder.encode(request.getPassword()))
                         .nickname(request.getNickname())
                         .phoneNumber(request.getPhoneNumber())
                         .authority(Authority.USER)
