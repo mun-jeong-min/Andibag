8312f5698ce518cb94c75e579d605f66c9144a2f
munjeongmin <3330kys@dsm.hs.kr>
2022-05-23 22:33:33 +0900

    [EDIT] securityConfig

--- andibdg/src/main/java/com/example/andibag/global/config/SecurityConfig.java
+++ andibdg/src/main/java/com/example/andibag/global/config/SecurityConfig.java
@@ -3,8 +3,12 @@ package com.example.andibag.global.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @RequiredArgsConstructor
 @Configuration
@@ -12,4 +16,26 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .cors().and()
+                .csrf().disable()
+                .formLogin().disable()
+
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                .and()
+                .authorizeRequests()
+
+                .anyRequest().permitAll();
+/*
+                .and()
+                .apply(new FilterConfig(jwtTokenProvider));
+                */
+    }
 }
