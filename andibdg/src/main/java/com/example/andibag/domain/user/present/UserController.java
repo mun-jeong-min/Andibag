036888ec5370c3ad71ba9faa71ec96991fab4f9a
munjeongmin <3330kys@dsm.hs.kr>
2022-05-23 22:53:26 +0900

    [EDIT] userSignup API add

--- andibdg/src/main/java/com/example/andibag/domain/user/present/UserController.java
+++ andibdg/src/main/java/com/example/andibag/domain/user/present/UserController.java
@@ -1,11 +1,23 @@
 package com.example.andibag.domain.user.present;
 
-import com.example.andibag.domain.user.service.UserSignInService;
+import com.example.andibag.domain.user.present.dto.request.SignUpRequest;
+import com.example.andibag.domain.user.service.UserSignUpService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/user")
 public class UserController {
-    private UserSignInService userSignInService;
+    private UserSignUpService userSignInService;
+
+    @PostMapping("/signup")
+    public void signup(@RequestBody @Valid SignUpRequest signUpRequest) {
+        userSignInService.signup(signUpRequest);
+    }
 }
--- andibdg/src/main/java/com/example/andibag/global/config/SecurityConfig.java
+++ andibdg/src/main/java/com/example/andibag/global/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.andibag.global.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -31,6 +32,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
                 .and()
                 .authorizeRequests()
+
+                .antMatchers(HttpMethod.POST, "/user/signup").permitAll()
 
                 .anyRequest().permitAll();
 /*
