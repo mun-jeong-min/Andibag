e65ac0c6231bd6bf99015457a185c7d3530597d8
munjeongmin <3330kys@dsm.hs.kr>
2022-05-24 18:34:50 +0900

    [ADD] RefreshToken Domain ADD

---
+++ andibdg/src/main/java/com/example/andibag/domain/auth/domain/RefreshToken.java
@@ -0,0 +1,31 @@
+package com.example.andibag.domain.auth.domain;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
+import org.springframework.data.redis.core.index.Indexed;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+@Getter
+@Builder
+@RedisHash
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class RefreshToken {
+    @Id
+    private String accountId;
+
+    @Indexed
+    private String token;
+
+    @TimeToLive
+    private Long timeToLive;
+
+    public void updateToken(String token) {
+        this.token = token;
+    }
+}
---
+++ andibdg/src/main/java/com/example/andibag/domain/auth/domain/repository/RefreshTokenRepository.java
@@ -0,0 +1,10 @@
+package com.example.andibag.domain.auth.domain.repository;
+
+import com.example.andibag.domain.auth.domain.RefreshToken;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String > {
+    Optional<RefreshToken> findByToken(String token);
+}
