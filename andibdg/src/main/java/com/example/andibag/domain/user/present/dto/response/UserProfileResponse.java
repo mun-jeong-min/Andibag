9df5dd5bb0cdc3a727e903ccc691e75662eb9de3
munjeongmin <3330kys@dsm.hs.kr>
2022-05-28 20:05:45 +0900

    ADD : UserProfileLogic add

---
+++ andibdg/src/main/java/com/example/andibag/domain/user/present/dto/response/UserProfileResponse.java
@@ -0,0 +1,11 @@
+package com.example.andibag.domain.user.present.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class UserProfileResponse {
+    private final String nickname;
+    private final String phoneNumber;
+}
---
+++ andibdg/src/main/java/com/example/andibag/domain/user/service/UserProfileService.java
@@ -0,0 +1,24 @@
+package com.example.andibag.domain.user.service;
+
+import com.example.andibag.domain.user.domain.User;
+import com.example.andibag.domain.user.domain.repository.UserRepository;
+import com.example.andibag.domain.user.exception.UserNotFoundException;
+import com.example.andibag.domain.user.present.dto.response.UserProfileResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserProfileService {
+    private final UserRepository userRepository;
+
+    public UserProfileResponse userProfile(Long id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> UserNotFoundException.EXCEPTION);
+
+        return UserProfileResponse.builder()
+                .nickname(user.getNickname())
+                .phoneNumber(user.getPhoneNumber())
+                .build();
+    }
+}
