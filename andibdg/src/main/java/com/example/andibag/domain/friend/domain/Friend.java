20b5d004a4a95347da9ebffb93f5c15afc0cdbc1
munjeongmin <3330kys@dsm.hs.kr>
2022-05-31 19:58:46 +0900

    ADD : Friend domain add

---
+++ andibdg/src/main/java/com/example/andibag/domain/friend/domain/Friend.java
@@ -0,0 +1,27 @@
+package com.example.andibag.domain.friend.domain;
+
+import com.example.andibag.domain.user.domain.User;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Friend {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Builder
+    public Friend(User user) {
+        this.user = user;
+    }
+}
---
+++ andibdg/src/main/java/com/example/andibag/domain/friend/domain/repository/FriendRepository.java
@@ -0,0 +1,7 @@
+package com.example.andibag.domain.friend.domain.repository;
+
+import com.example.andibag.domain.friend.domain.Friend;
+import org.springframework.data.repository.CrudRepository;
+
+public interface FriendRepository extends CrudRepository<Friend, Long> {
+}
