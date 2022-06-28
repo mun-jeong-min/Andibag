0fe4d10fdfc72b6b74360c8f572e01b514ba3241
munjeongmin <3330kys@dsm.hs.kr>
2022-06-28 15:48:26 +0900

    EDIT :: NoticeReadService

--- src/main/java/com/example/andibag/domain/notice/service/NoticeAllReadService.java
+++ src/main/java/com/example/andibag/domain/notice/service/NoticeAllReadService.java
@@ -1,10 +1,9 @@
 package com.example.andibag.domain.notice.service;
 
 import com.example.andibag.domain.notice.domain.repository.NoticeRepository;
-import com.example.andibag.domain.notice.present.dto.response.NoticeAllReadResponse;
-import com.example.andibag.domain.notice.present.dto.response.NoticeResponse;
+import com.example.andibag.domain.notice.domain.repository.type.NoticeCategory;
+import com.example.andibag.domain.notice.present.dto.response.QueryNoticeResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,12 +16,19 @@ public class NoticeAllReadService {
     private final NoticeRepository noticeRepository;
 
     @Transactional(readOnly = true)
-    public NoticeAllReadResponse noticeAllRead() {
-        List<NoticeResponse> noticeList = noticeRepository.findAll()
-                .stream().map(notice -> new NoticeResponse(
-                        notice.getId(), notice.getTitle(), notice.getContent(), notice.getCreateTime(), notice.getModifyTime()))
+    public QueryNoticeResponse noticeAllRead(NoticeCategory category) {
+        List<QueryNoticeResponse.NoticeQueryResponse> noticeList = noticeRepository.findNoticesByNoticeType(category)
+                .stream().map(notice -> QueryNoticeResponse.NoticeQueryResponse.builder()
+                        .id(notice.getId())
+                        .title(notice.getTitle())
+                        .content(notice.getContent())
+                        .createAt(notice.getCreateTime())
+                        .modifyAt(notice.getModifyTime())
+                        .noticeType(notice.getNoticeType().getName())
+                        .build()
+                )
                 .collect(Collectors.toList());
 
-        return new NoticeAllReadResponse(noticeList);
+        return new QueryNoticeResponse(noticeList);
     }
 }
