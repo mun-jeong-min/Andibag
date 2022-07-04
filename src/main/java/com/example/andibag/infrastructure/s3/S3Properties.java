93f95d333f3c7453d2f9b7c43bae16a4c890cb3c
munjeongmin <3330kys@dsm.hs.kr>
2022-07-04 22:01:27 +0900

    ADD :: infrastructure S3Util

---
+++ src/main/java/com/example/andibag/infrastructure/s3/S3Properties.java
@@ -0,0 +1,17 @@
+package com.example.andibag.infrastructure.s3;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
+
+@Getter
+@RequiredArgsConstructor
+@ConstructorBinding
+@ConfigurationProperties(prefix = "aws.s3")
+public class S3Properties {
+    private final String iamAccessKey;
+    private final String iamSecretKey;
+    private final String bucket;
+    private final String iamRegion;
+}
