29192bc594dcb02b91bf3d3e07e6f15681ab9737
munjeongmin <3330kys@dsm.hs.kr>
2022-07-05 23:11:15 +0900

    EDIT :: delete Friend

--- src/main/java/com/example/andibag/domain/friend/service/FriendSearchService.java
+++ src/main/java/com/example/andibag/domain/friend/service/FriendSearchService.java
@@ -1,17 +1,25 @@
 package com.example.andibag.domain.friend.service;
 
+import com.example.andibag.domain.friend.domain.Friend;
+import com.example.andibag.domain.friend.domain.repository.FriendRepository;
+import com.example.andibag.domain.friend.exception.FriendNotFoundException;
 import com.example.andibag.domain.friend.exception.PhoneMismatchException;
 import com.example.andibag.domain.friend.present.dto.request.FriendAddRequest;
 import com.example.andibag.domain.friend.present.dto.response.FriendResponse;
 import com.example.andibag.domain.user.domain.User;
 import com.example.andibag.domain.user.domain.repository.UserRepository;
+import com.example.andibag.domain.user.exception.UserNotFoundException;
+import com.example.andibag.domain.user.facade.UserFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
 public class FriendSearchService {
     private final UserRepository userRepository;
+    private final FriendRepository friendRepository;
+    private final UserFacade userFacade;
 
     public FriendResponse findUser(FriendAddRequest request) {
         User user = userRepository.findByPhoneNumber(request.getPhoneNumber())
@@ -22,5 +30,13 @@ public class FriendSearchService {
                 .nickname(user.getNickname())
                 .phoneNumber(user.getPhoneNumber())
                 .build();
+    }
+
+    @Transactional
+    public void deleteFriend(Long id) {
+        Friend friend = friendRepository.findById(id)
+                .orElseThrow(() -> FriendNotFoundException.EXCEPTION);
+
+        friendRepository.delete(friend);
     }
 }
