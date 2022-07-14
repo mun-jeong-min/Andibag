2218ab0d4560baf33c72601aea62a53bdf7b56e7
munjeongmin <3330kys@dsm.hs.kr>
2022-07-14 21:52:26 +0900

    ADD :: SocketConfig

---
+++ src/main/java/com/example/andibag/global/config/WebSocketConfig.java
@@ -0,0 +1,34 @@
+package com.example.andibag.global.config;
+
+import com.corundumstudio.socketio.Configuration;
+import com.corundumstudio.socketio.SocketIOServer;
+import com.example.andibag.global.socket.WebSocketMappingSuppoter;
+import com.example.andibag.global.socket.exception.SocketExceptionListener;
+import com.example.andibag.global.socket.security.WebSocketConnectController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class WebSocketConfig {
+    private final WebSocketMappingSuppoter mappingSuppoter;
+    private final WebSocketConnectController connectController;
+    private final SocketExceptionListener exceptionListener;
+
+    @Value("${socket.port}")
+    private Integer port;
+
+    @Bean
+    public SocketIOServer socketIOServer() {
+        Configuration config = new Configuration();
+        config.setPort(port);
+        config.setOrigin("*");
+        config.setExceptionListener(exceptionListener);
+        SocketIOServer server = new SocketIOServer(config);
+        mappingSuppoter.addListeners(server);
+        server.addConnectListener(connectController::onConnect);
+        return server;
+    }
+}
