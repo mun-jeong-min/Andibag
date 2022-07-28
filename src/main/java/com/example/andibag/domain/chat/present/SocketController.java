aacfc478d1f33a698829859cd5345384e0336544
munjeongmin <3330kys@dsm.hs.kr>
2022-07-28 09:06:32 +0900

    EDIT :: joinRoom controller

--- src/main/java/com/example/andibag/domain/chat/present/SocketController.java
+++ src/main/java/com/example/andibag/domain/chat/present/SocketController.java
@@ -1,13 +1,23 @@
 package com.example.andibag.domain.chat.present;
 
+import com.corundumstudio.socketio.SocketIOClient;
+import com.corundumstudio.socketio.SocketIOServer;
 import com.corundumstudio.socketio.annotation.OnEvent;
+import com.example.andibag.domain.chat.service.ChatRoomService;
 import com.example.andibag.global.socket.property.SocketProperty;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
+@RequiredArgsConstructor
 @RestController
 public class SocketController {
-    @OnEvent(SocketProperty.MESSAGE_KEY)
-    public String event() {
-        return "event";
+    private final ChatRoomService chatRoomService;
+
+    @OnEvent(SocketProperty.ROOM_KEY)
+    public void event(SocketIOServer server, SocketIOClient client, @RequestBody @Valid Long friendId) {
+        chatRoomService.joinRoom(server, client, friendId);
     }
 }
