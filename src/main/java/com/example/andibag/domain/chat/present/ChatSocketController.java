dfa2367fa0aaada6652f04c00ed28d804077cc78
munjeongmin <3330kys@dsm.hs.kr>
2022-07-31 23:23:44 +0900

    EDIT :: Socket Request Parameter

--- src/main/java/com/example/andibag/domain/chat/present/ChatSocketController.java
+++ src/main/java/com/example/andibag/domain/chat/present/ChatSocketController.java
@@ -2,7 +2,6 @@ package com.example.andibag.domain.chat.present;
 
 import com.corundumstudio.socketio.SocketIOClient;
 import com.corundumstudio.socketio.SocketIOServer;
-import com.example.andibag.domain.chat.present.dto.request.JoinRoomRequest;
 import com.example.andibag.domain.chat.service.ChatRoomService;
 import com.example.andibag.global.socket.annotation.SocketController;
 import com.example.andibag.global.socket.annotation.SocketMapping;
@@ -13,8 +12,8 @@ import lombok.RequiredArgsConstructor;
 public class ChatSocketController {
     private final ChatRoomService chatRoomService;
 
-    @SocketMapping(endpoint = "join", requestCls = JoinRoomRequest.class)
-    public void joinRoom(SocketIOClient client, SocketIOServer server, JoinRoomRequest request) {
-        chatRoomService.joinRoom(client, server, request);
+    @SocketMapping(endpoint = "join", requestCls = String.class)
+    public void joinRoom(SocketIOClient client, SocketIOServer server, String friendId) {
+        chatRoomService.joinRoom(client, server, Long.parseLong(friendId));
     }
 }
