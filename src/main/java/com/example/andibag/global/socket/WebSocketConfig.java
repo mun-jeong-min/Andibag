48f33930596566e85ef80b564788b3a089e54889
munjeongmin <3330kys@dsm.hs.kr>
2022-08-22 23:11:52 +0900

    EDIT :: MessageController

--- src/main/java/com/example/andibag/global/socket/WebSocketConfig.java
+++ src/main/java/com/example/andibag/global/socket/WebSocketConfig.java
@@ -3,24 +3,23 @@ package com.example.andibag.global.socket;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.WebSocketHandler;
-import org.springframework.web.socket.config.annotation.*;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
 @EnableWebSocket
 @RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-    private final WebSocketHandler webSocketHandler;
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/sub");
-        config.setApplicationDestinationPrefixes("/pub");
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/app");
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws-stomp").setAllowedOrigins("*")
-                .withSockJS();
+        registry.addEndpoint("/ws").setAllowedOrigins("*");
     }
 }
